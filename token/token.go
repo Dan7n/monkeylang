d8bd8b127898eb746903f1eae5147e5d3503aa1d
Danny Isaac <danny.isaac@klarna.com>
2024-10-18 17:33:33 +0200

    feat: add support for more keywords & update tests
    if, else, true, false, return

--- token/token.go
+++ token/token.go
@@ -38,11 +38,21 @@ const (
 	// keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"let": LET,
-	"fn":  FUNCTION,
+	"let":    LET,
+	"fn":     FUNCTION,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // returns either `keywords` if `ident` is a keyword, or the TokenType IDENT if its not
